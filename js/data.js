cae21a3e780380c51fab4720118af3de25741f17
Oliver Baptiste <github@oliverbaptiste.com>
2017-03-11 01:31:20 -0500

    Reorganizing data structure in separate data.js file for future integration

---
+++ js/data.js
@@ -0,0 +1,62 @@
+var suits = [
+  {
+    standard: { name: 'Spades', pip: '♠', color: 'black' },
+    tarot: { name: 'Swords' }
+  },
+  {
+    standard: { name: 'Hearts', pip: '♥', color: 'red' },
+    tarot: { name: 'Cups' }
+  },
+  {
+    standard: { name: 'Diamonds', pip: '♦', color: 'red' },
+    tarot: { name: 'Pentacles' }
+  },
+  {
+    standard: { name: 'Clubs', pip: '♣', color: 'black' },
+    tarot: { name: 'Wands' }
+  }
+];
+var ranks = [
+  {
+    pip: {
+      1: 'Ace',
+      2: 'Two',
+      3: 'Three',
+      4: 'Four',
+      5: 'Five',
+      6: 'Six',
+      7: 'Seven',
+      8: 'Eight',
+      9: 'Nine',
+      10: 'Ten'
+    },
+    court: {
+      standard: [ 'Jack', 'Queen', 'King' ],
+      tarot: [ 'Page', 'Knight', 'Queen', 'King' ]
+    }
+  }
+];
+var trumps =  [
+  { 0: 'The Fool' },
+  { 1: 'The Magician' },
+  { 2: 'The High Priestess' },
+  { 3: 'The Empress' },
+  { 4: 'The Emperor' },
+  { 5: 'The Hierophant' },
+  { 6: 'The Lovers' },
+  { 7: 'The Chariot' },
+  { 8: 'Strength' },
+  { 9: 'The Hermit' },
+  { 10: 'Wheel of Fortune' },
+  { 11: 'Justice' },
+  { 12: 'The Hanged Man' },
+  { 13: 'Death' },
+  { 14: 'Temperance' },
+  { 15: 'The Devil' },
+  { 16: 'The Tower' },
+  { 17: 'The Star' },
+  { 18: 'The Moon' },
+  { 19: 'The Sun' },
+  { 20: 'Judgement' },
+  { 21: 'The World' }
+];
